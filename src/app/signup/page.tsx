cbd18b1addcaf5ee2ebb79aabb87d6e423cf7111
VinaySohaliya <Sohaliyavin01@gmail.com>
2023-08-28 22:33:04 +0530

    frontend of signup and login page created

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,61 @@
+"use client"
+import Link from "next/link"
+import React from "react"
+import {useState } from 'react'
+import { useRouter } from "next/navigation"
+import axios from "axios"
+
+export default function signup() {
+    const [user, setuser] = useState({
+        name:"",
+        email:"",
+        password:"",
+    })
+ 
+    const signup=()=>{}
+
+    return (
+        <div className="flex justify-center items-center h-screen bg-black">
+            <div className="flex justify-center items-center   flex-col bg-black text-white
+            border-solid border-2 border-sky-500  p-8">
+          <div className="p-2 font-bold ">Signup</div>
+            <div className="mb-2 flex flex-col">
+              <label htmlFor="name">Name</label>
+              <input
+                id="name"
+                type="text"
+                value={user.name}
+                onChange={(e) => setuser({ ...user, name: e.target.value })}
+                className="p-2 border text-black w-70"
+              />
+            </div>
+            <div className="mb-2 flex flex-col">
+              <label htmlFor="email">Email</label>
+              <input
+                id="email"
+                type="email"
+                value={user.email}
+                onChange={(e) => setuser({ ...user, email: e.target.value })}
+                className=" w-70 p-2 border text-black"
+              />
+            </div>
+            <div className="mb-2 flex flex-col">
+              <label htmlFor="password">Password</label>
+              <input
+                id="password"
+                type="password"
+                value={user.password}
+                onChange={(e) => setuser({ ...user, password: e.target.value })}
+                className="w-70 p-2 border mb-2 text-black"
+              />
+            </div>
+            <button onClick={signup} type="submit" className="bg-blue-500 text-white p-2 mb-2 rounded">
+              Signup
+            </button>
+          <Link href="/login" className="mt-2 border-b-2 border-indigo-500 ">
+            Visit Login Page
+          </Link>
+        </div>
+        </div>
+      );
+}
---
+++ src/dbConfig/dbconfig.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+
+export async function connect(){
+    try {
+        mongoose.connect(process.env.MONGO_URL!)
+        const connection=mongoose.connection;
+        connection.on('connected',()=>{
+            console.log("mogodb connected succesfully");
+        })
+        connection.on('error',(err)=>{
+            console.log(err);
+            process.exit();
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
