514e3a69df35578389ca6fd44862df0115e2df6a
VinaySohaliya <Sohaliyavin01@gmail.com>
2023-08-29 22:06:42 +0530

    middleware created

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -1,49 +1,51 @@
 import mongoose from "mongoose";
 import { connect } from "@/dbConfig/dbconfig";
-import User from "@/models/usermodel"
-import { NextRequest,NextResponse } from "next/server";
-import bcryptjs from 'bcryptjs'
-import jwt from 'jsonwebtoken'
+import User from "@/models/usermodel";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from 'bcryptjs';
+import jwt from "jsonwebtoken";
+import dotenv from 'dotenv'; 
 
-connect()
+dotenv.config();
 
-export async function POST(request:NextRequest) {
-    try {
-
-        const reqBody= await request.json();
-        console.log(reqBody);
-        const {email,password}=reqBody;
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { email, password } = reqBody;
         
-        if (!email||!password) {
-            return NextResponse.json({message:"All feild Are required"},{status:400});
-        } 
+    if (!email || !password) {
+      return NextResponse.json({ message: "All fields are required" }, { status: 400 });
+    } 
         
-        const user= await User.findOne({email});
-        if (!user) {
-        return NextResponse.json({message:"user not find"},{status:404})  
-        }
-
-        const tokenData={
-            id:user._id,
-            name:user.name,
-            email:user.email
-        }
-        const token =await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {expireIn:"1d"});
-        const res=NextResponse.json({
-            message:"login Succesfully",
-            success:true
-        })
-
-        console.log(res);
-        res.cookies.set("token",token,{httpOnly:true})
-
-        return res;
-        
-
-        
-        
-        
-    } catch (error:any) {
-        return NextResponse.json({message:error.message},{status:500})
+    const user = await User.findOne({ email });
+    if (!user) {
+      return NextResponse.json({ message: "User not found" }, { status: 404 });  
     }
+
+    const validPassword = await bcryptjs.compare(password, user.password);
+    if (!validPassword) {
+      return NextResponse.json({
+        message: "Invalid password",
+        success: false
+      });
+    }
+
+    const tokenData = {
+      id: user._id,
+      name: user.name,
+      email: user.email
+    };
+
+    const token = await jwt.sign(tokenData, 'process.env.TOKEN_SECRET', { expiresIn: "1d" });
+    const res = NextResponse.json({
+      message: "Login successfully",
+      success: true
+    });
+
+    res.cookies.set("token", token, { httpOnly: true });
+
+    return res;
+  } catch (error: any) {
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
 }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,28 @@
+"use client"
+
+import axios from "axios"
+import {useRouter} from "next/navigation"
+import { toast } from "react-hot-toast"
+
 export default function userProfile() {
+    const router=useRouter()
+    const logout=async()=>{
+        try {            
+            await axios.get("/api/users/logout")
+            toast.success('logout Succesfully')
+            router.push('/login')
+        } catch (error:any) {
+         toast.error("logout Fail") 
+         console.log(error.message);
+           
+        }
+    }
     return(
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
             <hr />
             <p className=" text-4xl"></p>
+            <button onClick={logout} className="rounded bg-blue-500 text-white p-2 mb-2">Logout</button>
         </div>
     )
 }
